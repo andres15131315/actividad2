47bc2d1eb4a21441b6f619db0e63804002358fcd
andres santiago <asve1315@gmail.com>
2025-09-25 00:42:08 +0200

    first commit

--- src/app/productos/productos.ts
+++ src/app/productos/productos.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface Product {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  image: string;
+}
 
 @Component({
   selector: 'app-productos',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './productos.html',
   styleUrls: ['./productos.css']
 })
-export class Productos {}
+export class Productos {
+  products: Product[] = [
+    { id: 1, name: 'loro', description: 'loro de montaña para todos los terrenos', price: 1200, image: '/img/descarga2.jpg' },
+    { id: 2, name: 'tigre', description: 'tigre de montaña para todos los terrenos', price: 150, image: '/img/descarga1.jpg' },
+    { id: 3, name: 'leon', description: 'leon de montaña para todos los terrenos', price: 50, image: '/img/descarga3.jpg' }
+  ];
+}
